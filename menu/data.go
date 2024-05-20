5fe3b53644ab5c665a91fada5fe8a0fbc9b4ba50
Hafsa R. <hafsarodriguez@gmail.com>
2024-05-19 22:54:21 -0400

    update

---
+++ menu/data.go
@@ -0,0 +1,14 @@
+package menu
+
+var Menu = []menuItem{
+	{name: "Coffee", price: map[string]float64{
+		"small":  3.50,
+		"medium": 4.50,
+		"large":  5.50,
+	}},
+	{name: "Tea", price: map[string]float64{
+		"small":  2.50,
+		"medium": 3.50,
+		"large":  4.50,
+	}},
+}
